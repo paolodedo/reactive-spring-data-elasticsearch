efd5850bdc3aaf9e5e796ad4de36d769314334b8
Paolo De Dominicis <pdedominicis@key-biz.it>
2020-03-24 16:23:38 +0100

    no message

--- src/main/java/it/paolodedo/reactivespringdataelasticsearch/ReactiveSpringDataElasticsearchApplication.java
+++ src/main/java/it/paolodedo/reactivespringdataelasticsearch/ReactiveSpringDataElasticsearchApplication.java
@@ -2,10 +2,8 @@ package it.paolodedo.reactivespringdataelasticsearch;
 
 import it.paolodedo.reactivespringdataelasticsearch.model.MyModel;
 import it.paolodedo.reactivespringdataelasticsearch.service.MyModelService;
-import it.paolodedo.reactivespringdataelasticsearch.service.impl.MyModelServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Mono;
