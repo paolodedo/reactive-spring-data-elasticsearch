4afc02e85cb2863c7d911027f299c6ad1e36f304
Paolo De Dominicis <pdedominicis@key-biz.it>
2020-03-24 19:09:22 +0100

    Added test class.

--- src/main/java/it/paolodedo/reactivespringdataelasticsearch/ReactiveSpringDataElasticsearchApplication.java
+++ src/main/java/it/paolodedo/reactivespringdataelasticsearch/ReactiveSpringDataElasticsearchApplication.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class ReactiveSpringDataElasticsearchApplication {
@@ -28,41 +30,52 @@ public class ReactiveSpringDataElasticsearchApplication {
 
 	@PostConstruct
 	public void test() {
+
+		// Delay to allow index to be created by MyModelServiceImpl's @PostConstruct annotated method
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+		}
+
 		// SAVE
 		MyModel myModel = new MyModel();
 		myModel.setData("test");
 		myModel.setTimestamp(new Date().getTime());
-		Mono<MyModel> saveMono = myModelService.saveMyModel(myModel);
 
-		saveMono.subscribe(savedObject -> {
-			logger.info("Object persisted: {}", savedObject);
-
-			// UPDATE
-			myModel.setData("test UPDATED");
-			myModel.setTimestamp(new Date().getTime());
-			myModel.setId(savedObject.getId());
-
-			myModelService.saveMyModel(myModel)
-				.subscribe(updatedObject -> {
-					logger.info("Object updated: {}", updatedObject);
-
-					// FIND ALL FILTERED BY FIELD
-					myModelService.findAllMyModels("data", "test UPDATED")
-						.subscribe(findAllResult -> logger.info("FindAll result: {}", findAllResult));
-
-					// FIND BY ID
-					myModelService.findMyModelById(updatedObject.getId())
-						.subscribe(foundObject -> {
-							logger.info("Object found: {}", foundObject);
-
-							// DELETE
-							myModelService.deleteMyModelById(foundObject.getId())
-								.subscribe(deletedObjectId -> {
-									logger.info("Object deleted's ID: {}", deletedObjectId);
-								});
-						});
-				});
-		});
+		myModelService.saveMyModel(myModel)
+			.doOnNext(savedObject -> logger.info("Object persisted: {}", savedObject))
+			.delayElement(Duration.ofSeconds(2))
+			.map(savedObject -> {
+				// FIND ALL FILTERED BY FIELD
+				return myModelService.findAllMyModels("data", "test");
+			})
+			.flatMapMany(Function.identity())
+			.delayElements(Duration.ofSeconds(2))
+			.doOnNext(objectRetrieved -> logger.info("Objects retrieved by find all: {}", objectRetrieved))
+			.map(objectRetrieved -> {
+				// UPDATE
+				objectRetrieved.setData("test UPDATED");
+				objectRetrieved.setTimestamp(new Date().getTime());
+				return myModelService.saveMyModel(objectRetrieved);
+			})
+			.flatMap(Function.identity())
+			.delayElements(Duration.ofSeconds(2))
+			.doOnNext(updatedObject -> logger.info("Object updated: {}", updatedObject))
+			.map(updatedObject -> {
+				// FIND BY ID
+				return myModelService.findMyModelById(updatedObject.getId());
+			})
+			.flatMap(Function.identity())
+			.delayElements(Duration.ofSeconds(2))
+			.doOnNext(foundObject -> logger.info("Object found: {}", foundObject))
+			.map(foundObject -> {
+				// DELETE
+				return myModelService.deleteMyModelById(foundObject.getId());
+			})
+			.flatMap(Function.identity())
+			.delayElements(Duration.ofSeconds(2))
+			.doOnNext(deletedObjectId -> logger.info("Object deleted's ID: {}", deletedObjectId))
+			.subscribe();
 	}
 
 
